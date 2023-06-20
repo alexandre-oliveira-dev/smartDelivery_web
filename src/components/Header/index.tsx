a6778427f1f9caad4e0d4e0033fbe09831677ec2
Alexandretecsomobi <alexandredev.frontend@gmail.com>
2023-06-20 20:06:39 -0300

    add sistem login

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { Button, Form, Input, Modal, Typography } from "antd";
+import FormItem from "antd/es/form/FormItem";
+import { api } from "../../services/api";
+
+type SinginTypes = {
+  email: string | undefined;
+  password: string | undefined;
+};
 
 export default function Header() {
+  const [openModal, setOpenModal] = useState(false);
+  const [errmessage, setErrmessage] = useState("");
+  const [dataform, setDataform] = useState<SinginTypes>();
+
+  const [form] = Form.useForm();
+
+  if (dataform) {
+    async function handleLogin() {
+      console.log(dataform);
+
+      if (!form.getFieldValue("email") || !form.getFieldValue("password") || !dataform) {
+        setErrmessage("Preencha todos os campos");
+        return;
+      }
+
+      await api
+        .post("/singin", {
+          email: dataform?.email,
+          password: dataform?.password,
+        })
+        .then((response) => {
+          localStorage.setItem("@sessionDelivery", JSON.stringify(response.data));
+
+          window.location.href = `/dashboard/${response.data?.name_company}`;
+        })
+        .catch((err) => {
+          document.getElementById("err")?.setAttribute("style", "display:flex");
+          setErrmessage("Dados inválidos ou não cadastrados!");
+          console.log(err);
+        });
+    }
+    handleLogin();
+  }
+
   return (
     <header className="box-header">
       <div className="header">
-          <div>logoaqui</div>
-          <div className='nav-btns'>
-            <button>Inicio</button>
-            <button>Quem somos</button>
-            <button>Duvidas</button>
+        <div>logoaqui</div>
+        <div className="nav-btns">
+          <button>Inicio</button>
+          <button>Quem somos</button>
+          <button>Duvidas</button>
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div
+            style={{
+              position: "relative",
+            }}
+          >
+            <Button onClick={() => setOpenModal(true)}>Entrar</Button>
+            <Modal
+              closable={false}
+              style={{ padding: "20px" }}
+              open={openModal}
+              okText="Entrar"
+              onCancel={() => setOpenModal(false)}
+              onOk={() => {
+                if (!form.getFieldValue("email") && !form.getFieldValue("password")) {
+                  setErrmessage("Preencha todos os campos");
+                  return;
+                }
+                form.submit();
+              }}
+            >
+              <Typography.Title level={3}>Fazer login</Typography.Title>
+              <Form form={form} onFinish={(data) => setDataform(data)}>
+                <FormItem name={"email"}>
+                  <Input placeholder="Digite seu email"></Input>
+                </FormItem>
+                <FormItem name={"password"}>
+                  <Input placeholder="Digite sua senha"></Input>
+                </FormItem>
+                <Button type="link" href="#">
+                  Esqueci minha senha
+                </Button>
+                <span id="err">{errmessage}</span>
+              </Form>
+            </Modal>
           </div>
-          <div>
-            <a href="/login">Entrar</a>
-            <a href="/cadastro">Cadastre-se</a>
-          </div>
+          <a href="/cadastro">Cadastre-se</a>
+        </div>
       </div>
     </header>
   );
--- src/pages/Dashboard/components/navbarComponent/index.tsx
+++ src/pages/Dashboard/components/navbarComponent/index.tsx
@@ -1,38 +1,98 @@
-import React from  'react'
-import './style.css'
-import { MdLogout } from 'react-icons/md'
+import React, { useEffect, useState, useContext } from "react";
+import "./style.css";
+import { MdLogout } from "react-icons/md";
+import { api } from "../../../../services/api";
+import { IsLoginContext, LoginContext } from "../../../../context/isLogin.context";
 
-type Activebtntype={
-    btn1?:boolean
-    btn2?:boolean
-    btn3?:boolean
-}
+type Activebtntype = {
+  btn1?: boolean;
+  btn2?: boolean;
+  btn3?: boolean;
+};
 
-export default function NavBarComponent({btn1,btn2,btn3}:Activebtntype){
+type UserCredencials = {
+  email: string | undefined;
+  name_company: string | undefined;
+  id: string | undefined;
+};
 
-    const navBarBtns = [
-        {id:3, title:'Dashboard',link:`/dashboard/${'nomerest'}/faturamento`,icon:'',active:btn2},
-        {id:2, title:'Meus Pedidos',link:`/dashboard/${'nomerest'}`,icon:'',active:btn1},
-        {id:3, title:'Configurações',link:`/dashboard/${'nomerest'}/config`,icon:'',active:btn3}
-    ]
+export default function NavBarComponent({ btn1, btn2, btn3 }: Activebtntype) {
+  const [datUser, setDataUser] = useState<UserCredencials>();
 
-    return(
-        <nav className='navbardash'>
-            <div style={{display:"flex",gap:"10px",alignItems:"center",margin:"0 0 20px 0"}}>
-                <div className='profile'></div>
-                <p>{'username'}</p>
-            </div>
-          <div className='boxbtnnavdash'>
-          {
-            navBarBtns.map(item => {
-                return(
-                    <button type='button' onClick={(()=>window.location.href=item.link)} style={item.active ? { color:"silver",fontWeight:"600"} : {}} key={item.id}>{item.icon} {item.title}</button>
-                )
-            })
-          }
-          </div>
+  const {asUser} = useContext(IsLoginContext);
 
-          <button className='btn-sair'><MdLogout></MdLogout> sair</button>
-        </nav>
-    )
+  console.log(asUser)
+
+  useEffect(() => {
+
+    setDataUser(JSON.parse(localStorage.getItem("@sessionDelivery") as any));
+  }, [asUser]);
+
+  async function handleSingout() {
+    await api.delete(`/singout/${datUser?.id}`).then(() => {
+      localStorage.removeItem("@sessionDelivery");
+      window.location.href = "/";
+    });
+  }
+
+  const navBarBtns = [
+    {
+      id: 3,
+      title: "Dashboard",
+      link: `/dashboard/${datUser?.name_company}/faturamento`,
+      icon: "",
+      active: btn2,
+    },
+    {
+      id: 2,
+      title: "Meus Pedidos",
+      link: `/dashboard/${datUser?.name_company}`,
+      icon: "",
+      active: btn1,
+    },
+    {
+      id: 3,
+      title: "Configurações",
+      link: `/dashboard/${datUser?.name_company}/config`,
+      icon: "",
+      active: btn3,
+    },
+  ];
+
+  return (
+    <LoginContext>
+      <nav className="navbardash">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            alignItems: "center",
+            margin: "0 0 20px 0",
+          }}
+        >
+          <div className="profile"></div>
+          <p style={{ color: "#fff", fontSize: "17px" }}>Bem vindo {datUser?.name_company}</p>
+        </div>
+        <div className="boxbtnnavdash">
+          {navBarBtns.map((item) => {
+            return (
+              <button
+                type="button"
+                onClick={() => (window.location.href = item.link)}
+                style={item.active ? { color: "silver", fontWeight: "600" } : {}}
+                key={item.id}
+              >
+                {item.icon} {item.title}
+              </button>
+            );
+          })}
+        </div>
+
+        <button className="btn-sair" onClick={handleSingout}>
+          <MdLogout></MdLogout> sair
+        </button>
+      </nav>
+    </LoginContext>
+  );
 }
