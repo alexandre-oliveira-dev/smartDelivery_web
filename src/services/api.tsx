9085ebdcb4e85bca3afa868ef924f3e523490afc
Alexandretecsomobi <alexandredev.frontend@gmail.com>
2023-09-02 22:31:41 -0300

    update

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const patchRoute = window.location.origin;
 
+  export const api = axios.create({
+    baseURL: patchRoute.includes('localhost')
+      ? 'https://smartdelivery-api-dev.onrender.com'
+      : 'https://project-delivery-api.onrender.com',
+  });
 /* export const api = axios.create({
-  baseURL: patchRoute.includes('localhost')
-    ? 'http://localhost:3333'
-    : 'https://project-delivery-api.onrender.com',
-}); */
-export const api = axios.create({
   baseURL: 'https://project-delivery-api.onrender.com',
-});
+}); */
