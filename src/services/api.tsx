bd56eb5abe11835c0bae0c1451a5e34f7545e592
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2024-01-16 08:50:25 -0300

    checkpoint

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,11 +3,11 @@ import axios from "axios";
 const patchRoute = window.location.origin;
 
 
-export const api = axios.create({
+/* export const api = axios.create({
   baseURL: patchRoute.includes('localhost')
     ? process.env.REACT_APP_API_URL_DEV
     : process.env.REACT_APP_API_URL_PROD,
-});
-/* export const api = axios.create({
-  baseURL: 'https://project-delivery-api.onrender.com',
 }); */
+export const api = axios.create({
+  baseURL: 'https://project-delivery-api.onrender.com',
+});
